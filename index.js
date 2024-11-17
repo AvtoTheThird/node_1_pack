04e3648b9639ccfc97925e9894dbdffd18fa7043
AvtoTheThird <avtonariashvili@gmail.com>
2024-11-17 02:04:21 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const packageInfo = require('./package.json');
+console.log(`Greetings! Running version ${packageInfo.version} of the app ${packageInfo.name}`);
+
+function ReversePolishNotation(input) {
+    const tokens = input.split(" ");
+    const stack = [];
+
+    tokens.forEach(token => {
+        if (!isNaN(token)) {
+            stack.push(Number(token));
+        } else {
+
+            const b = stack.pop();
+            const a = stack.pop();
+
+            switch (token) {
+                case '+':
+                    stack.push(a + b);
+                    break;
+                case '-':
+                    stack.push(a - b);
+                    break;
+                case '*':
+                    stack.push(a * b);
+                    break;
+                case '/':
+                    stack.push(a / b);
+                    break;
+                default:
+                    throw new Error(`Unknown operator: ${token}`);
+            }
+        }
+    });
+
+    return stack.pop();
+}
+module.exports = ReversePolishNotation;
